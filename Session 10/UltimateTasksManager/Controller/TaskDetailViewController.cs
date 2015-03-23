310714324f66e1c2bbe2fdb0c31e2f78a5bcfeb2
Martin Pernica <pernica@pria.cz>
2015-03-23 14:55:51 +0100

    Added source code from session 10

---
+++ Session 10/UltimateTasksManager/Controller/TaskDetailViewController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using UltimateTasksManager.Model;
+using System.Threading.Tasks;
+
+namespace UltimateTasksManager.Controller
+{
+	[Register ("TaskDetailViewController")]
+	partial class TaskDetailViewController : UIViewController
+	{
+		public TaskEntity SelectedTask { get; set; }
+		public TasksRepository TasksModel;
+
+		private WeatherModel WeatherAPI;
+
+		public TaskDetailViewController (IntPtr ptr) : base (ptr)
+		{
+			WeatherAPI = new WeatherModel ();
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+
+			// Set data from item
+			if (SelectedTask != null) {
+				TitleLabel.Text = SelectedTask.Title;
+
+				if (SelectedTask.State) {
+					TitleLabel.TextColor = UIColor.Green;
+				}
+
+				DescriptionLabel.Text = SelectedTask.Description;
+				DueDateLabel.Text = SelectedTask.DueDate.ToLongDateString ();
+				// TODO: Show Priority
+
+				// Load weather data
+				LoadWeather();
+			} else {
+				throw new ArgumentException ("You must set SelectedTask property to show detail view!");
+			}
+		}
+
+		private async void LoadWeather()
+		{
+			if (String.IsNullOrWhiteSpace (SelectedTask.Location)) {
+				ForecastLabel.Text = "Unknown location";
+				return;
+			}
+
+			try{
+				Weather condition = await WeatherAPI.FetchWeatherForLocationAsync(SelectedTask.Location);
+				ForecastLabel.Text = String.Format ("{0} {1}°C", condition.Text, condition.Temperature);
+			}catch(WeatherModelException ex) {
+				ForecastLabel.Text = "Forecast unavailable...";
+
+#if DEBUG
+				ForecastLabel.Text = ex.Message;
+#endif
+
+				ForecastLabel.TextColor = UIColor.Red;
+			}
+		}
+
+		partial void DeleteButtonTapped (NSObject sender)
+		{
+			string[] buttons = { "Yes" };
+			UIAlertView alert = new UIAlertView ("Confirmation", "Are you sure to delete this task?", null, "No", buttons);
+
+			alert.Clicked += (object alertSender, UIButtonEventArgs e) => {
+				switch (e.ButtonIndex) {
+				case 0:
+					// No
+					break;
+				case 1:
+					// Yes
+					TasksModel.Delete (SelectedTask);
+
+					NavigationController.PopViewController (true);
+					break;
+				}
+			};
+
+			alert.Show ();
+		}
+	}
+}
+
---
+++ Session 10/UltimateTasksManager/Model/GenericSqliteStorage.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace UltimateTasksManager
+{
+	public class GenericSqliteStorage<T> where T : new()
+	{
+		public string DatabaseVersionString = "v1";
+		protected string PathToDatabase;
+		protected SQLiteAsyncConnection ConnectionAsync;
+
+		public GenericSqliteStorage ()
+		{
+			string documents = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			PathToDatabase = Path.Combine(documents, String.Format("Database_{0}.db", DatabaseVersionString));
+		}
+
+		~GenericSqliteStorage ()
+		{
+			CloseAllConnections ();
+		}
+
+		public void CreateConnection ()
+		{
+			ConnectionAsync = new SQLiteAsyncConnection (PathToDatabase);
+		}
+
+		public void CloseAllConnections ()
+		{
+			SQLiteConnectionPool.Shared.Reset ();
+		}
+
+		public bool IsDatabaseFileExist()
+		{
+			return File.Exists (PathToDatabase);
+		}
+
+		public async Task CreateDatabase()
+		{
+			await ConnectionAsync.CreateTableAsync<T> ();
+		}
+
+		public async Task<List<T>> GetAll ()
+		{
+			AsyncTableQuery<T> tasksTable = ConnectionAsync.Table<T> ();
+			List<T> list = await tasksTable.ToListAsync ();
+
+			return list;
+		}
+
+		public async Task<int> Add (T task)
+		{
+			int id = await ConnectionAsync.InsertAsync (task);
+
+			return id;
+		}
+
+		public async void Delete (T task)
+		{
+			// No need to wait
+			await ConnectionAsync.DeleteAsync (task);
+		}
+
+		public async void Update (T task)
+		{
+			// no need to wait
+			await ConnectionAsync.UpdateAsync (task);
+		}
+	}
+}
+
